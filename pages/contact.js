211f8e392030c2b6223158b297f987006f3e1e83
victordu45 <54636055+victordu45@users.noreply.github.com>
2022-06-19 15:19:21 +0200

    Correctif design page contact et page validation message envoyé

--- pages/contact.js
+++ pages/contact.js
@@ -1,39 +1,57 @@
 import React from 'react';
+import Link from "next/link";
 import { useForm, ValidationError } from '@formspree/react';
 function ContactForm() {
     const [state, handleSubmit] = useForm("moqrbzov");
     if (state.succeeded) {
-        return <p>Message envoyé</p>;
+        return (
+            <div className='containerMessage'>
+                <p className='message'>Message envoyé</p>
+                <div className='lienContact'>
+                    <Link href={"https://dmfront-victordu45.vercel.app"}>
+                        <div>Retour à l'accueil</div>
+                    </Link>
+                </div>
+            </div>
+        );
     }
     return (
         <div className="bodyContact">
+            <div className="raceTitle">Formulaire de contact</div>
             <form className='formulaireContact' onSubmit={handleSubmit}>
-                <label htmlFor="email">
-                    Email Address
-                </label>
-                <input
-                    id="email"
-                    type="email"
-                    name="email"
-                    placeholder='email'
-                />
-                <ValidationError
-                    prefix="Email"
-                    field="email"
-                    errors={state.errors}
-                />
-                <textarea
-                    id="message"
-                    name="message"
-                    placeholder='Votre message'
-                />
-                <ValidationError
-                    prefix="Message"
-                    field="message"
-                    errors={state.errors}
-                />
+                <div className='input'>
+                    <label htmlFor="email">
+                        Adresse Email
+                    </label>
+                    <input
+                        id="email"
+                        type="email"
+                        name="email"
+                        placeholder='email'
+                    />
+                    <ValidationError
+                        prefix="Email"
+                        field="email"
+                        errors={state.errors}
+                    />
+                </div>
+                <div className='input'>
+                    <label htmlFor="message">
+                        Votre message
+                    </label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        placeholder='Votre message'
+                    />
+                    <ValidationError
+                        prefix="Message"
+                        field="message"
+                        errors={state.errors}
+                    />
+                </div>
                 <button className='buttonContact' type="submit" disabled={state.submitting}>
-                    Submit
+                    Envoyer
                 </button>
             </form>
         </div>
